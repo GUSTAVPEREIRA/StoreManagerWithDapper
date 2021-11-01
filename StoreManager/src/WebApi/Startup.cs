091fe3997e10fb76370cd8984d031156ed0e2707
pereirovisk <gugupereira123@hotmail.com>
2021-11-01 14:00:52 -0300

    feat(1): Created Role Completed Crud flow.

--- StoreManager/src/WebApi/Startup.cs
+++ StoreManager/src/WebApi/Startup.cs
@@ -22,6 +22,8 @@ namespace WebApi
             services.AddSwagger();
             services.AddMigrations(Configuration);
             services.AddDependencyInjection();
+            services.AddAutoMapper();
+            services.AddFluentValidation();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
