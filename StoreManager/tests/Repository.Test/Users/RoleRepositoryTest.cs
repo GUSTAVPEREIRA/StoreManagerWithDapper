6383e3b3e7461649a22c90f356f7c6732b00e2f4
pereirovisk <gugupereira123@hotmail.com>
2021-11-01 14:00:52 -0300

    feat(#1): Created Role Completed Crud flow.

--- StoreManager/tests/Repository.Test/Users/RoleRepositoryTest.cs
+++ StoreManager/tests/Repository.Test/Users/RoleRepositoryTest.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Bogus;
+using Dummie.Test.Users;
 using FluentAssertions;
 using Infrastructure.Users;
 using Repository.Test.Configuration;
@@ -8,38 +11,105 @@ using Xunit;
 
 namespace Repository.Test.Users
 {
-    public class RoleRepositoryTest : IDisposable
+    public sealed class RoleRepositoryTest : IDisposable
     {
         private readonly RoleRepository _roleRepository;
-        private readonly string _databaseName = "rolesDatabase";
+        private const string DatabaseName = "rolesDatabase";
+        private readonly RoleSeeder _seeder;
 
         public RoleRepositoryTest()
         {
-            var configuration = new RepositoryTestConfiguration().CreateConfigurations(_databaseName);
-            DatabaseConfiguration.CreateMigrations(_databaseName);
+            var configuration = new RepositoryTestConfiguration().CreateConfigurations(DatabaseName);
+            DatabaseConfiguration.CreateMigrations(DatabaseName);
             _roleRepository = new RoleRepository(configuration, new SqLiteDbConnectionProvider());
+            _seeder = new RoleSeeder(_roleRepository);
         }
 
         [Fact]
-        public async Task GetUserOk()
+        public async Task GetRolesOk()
         {
-            var seeder = new RoleSeeder(_roleRepository);
             var count = new Random().Next(1, 10);
-            var stubRoles = await seeder.CreateRoles(count);
+            var roles = await _seeder.CreateRoles(count);
 
-            for (var i = 0; i < count; i++)
-            {
-                stubRoles[i].Id = i + 1;
-            }
+            var result = await _roleRepository.GetRolesAsync();
 
-            var roles = await _roleRepository.GetRolesAsync();
-
-            roles.Should().BeEquivalentTo(stubRoles);
+            result.Should().BeEquivalentTo(roles);
         }
 
+        [Fact]
+        public async Task GetRolesNotFound()
+        {
+            var roles = await _roleRepository.GetRolesAsync();
+            roles.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task InsertRoleOk()
+        {
+            var newRole = new RoleDummie().Generate();
+
+            var result = await _roleRepository.CreateRoleAsync(newRole);
+            newRole.Id = result.Id;
+
+            result.Should().BeEquivalentTo(newRole);
+        }
+
+        [Fact]
+        public async Task DeleteRoleOk()
+        {
+            var roles = await _seeder.CreateRoles(1);
+            var role = roles.First();
+
+            await _roleRepository.DeleteRoleAsync(role.Id);
+            var result = await _roleRepository.GetRolesAsync();
+
+            result.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task UpdateRoleOk()
+        {
+            var count = new Random().Next(1, 10);
+            var roles = await _seeder.CreateRoles(count);
+
+            var faker = new Faker();
+
+            foreach (var role in roles)
+            {
+                role.Name = faker.Person.FullName;
+                role.IsAdmin = faker.Random.Bool();
+
+                await _roleRepository.UpdateRoleAsync(role);
+            }
+
+            var results = await _roleRepository.GetRolesAsync();
+
+            results.Should().BeEquivalentTo(roles);
+        }
+
+        [Fact]
+        public async Task GetRoleAsyncOk()
+        {
+            var count = new Random().Next(1, 10);
+            var roles = await _seeder.CreateRoles(count);
+            var role = roles.First(x => x.Id == count);
+
+            var result = await _roleRepository.GetRoleAsync(role.Id);
+
+            result.Should().BeEquivalentTo(role);
+        }
+        
+        [Fact]
+        public async Task GetRoleAsyncNotFound()
+        {
+            var result = await _roleRepository.GetRoleAsync(1);
+
+            result.Should().BeNull();
+        }
+        
         public void Dispose()
         {
-            DatabaseConfiguration.RemoveMigrations(_databaseName);
+            DatabaseConfiguration.RemoveMigrations(DatabaseName);
         }
     }
 }
